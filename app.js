4e99f41892205d46de84695a85bf38c22c6ffdcc
AndrewDaw <andrew.daw1995@gmail.com>
2019-08-01 18:44:06 +1200

    app now gets a basic current summary of weather in wellington

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-console.log('Starting')
+const request = require('request')
+const tempConvert = require('temp-units-conv')
+//open weather api key required 
+const key = ''
 
-setTimeout(()=> {
-    console.log('2 second timer')
-}, 2000)
 
-setTimeout(()=> {
-    console.log('0 second timer')
-}, 0)
+const url = 'http://api.openweathermap.org/data/2.5/weather?lat=-41.2865&lon=174.7762&appid='+key
 
-console.log('Stopping')
+request({ url: url, json: true}, (error, response)=>{
+    console.log('It is currently '+
+    tempConvert.k2c(response.body.main.temp).toFixed(2) +
+    ' degrees celsius with '+
+    response.body.weather[0].description +
+    ' in '+
+    response.body.name)
+})
