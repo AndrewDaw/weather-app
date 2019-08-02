bdc23b18584ddef79a2e2abe243d10deb9640e9f
AndrewDaw <andrew.daw1995@gmail.com>
2019-08-02 17:32:53 +1200

    Added geocoding api to search for coordinates to pass to weather api from city names. Begin code refactoring

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const request = require('request')
 const tempConvert = require('temp-units-conv')
+const geocode = require('./utils/geocode')
 //open weather api key required 
-const key = ''
+const weatherKey = ''
+const mapboxKey = ''
+const language = 'en'
 
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?lat=-41.2865&lon=174.7762&appid='+key
+// const weatherUrl = 
+// 'http://api.openweathermap.org/data/2.5/weather?lat=-41.2865&lon=174.7762&appid=' +
+// weatherKey +
+// '&units=metric' +
+// '&lang=' +
+// language
 
-request({ url: url, json: true}, (error, response)=>{
-    console.log('It is currently '+
-    tempConvert.k2c(response.body.main.temp).toFixed(2) +
-    ' degrees celsius with '+
-    response.body.weather[0].description +
-    ' in '+
-    response.body.name)
+// request({ url: weatherUrl, json: true}, (error, response)=>{
+//     if(error){
+//         console.log('Unable to get weather')
+//     } else if (response.body.cod === '400'){
+//         console.log('Unable to find location')
+//     }else{
+//         console.log('It is currently '+
+//         response.body.main.temp +
+//         ' degrees celsius with '+
+//         response.body.weather[0].description +
+//         ' in '+
+//         response.body.name)
+//     }
+    
+// })
+
+
+
+
+
+geocode('Wellington NZ', mapboxKey, language, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
---
+++ utils/geocode.js
@@ -0,0 +1,40 @@
+/*Geocode api
+*Geocode geocode function
+*address: Search term e.g. London England
+*mapBoxKey: api key. aquire from mapbox.com
+*language: prefferred language of result e.g. en
+*callback:function that will be called when results are available
+*/
+
+
+const request = require('request')
+
+const geocode = (address, mapboxKey, language, callback) => {
+    const mapBoxUrl = 
+    'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+    encodeURIComponent(address) +
+    '.json?access_token=' +
+    encodeURIComponent(mapboxKey) +
+    '&language=' +
+    encodeURIComponent(language)
+
+
+
+    request({ url: mapBoxUrl, json: true}, (error, response)=>{
+
+        if(error){
+          callback('Unable to get connection', undefined)
+        } else if (response.body.features.length == 0){
+         callback('Unable to find location', undefined)
+        }else{
+            callback(undefined,{
+                lat: response.body.features[0].center[1],
+                lng: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+
+}
+
+module.exports = geocode
